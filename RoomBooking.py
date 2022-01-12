97234d4e6e3b7bb991fa29754330130d45f8c351
rssekhar <93420682+rssekhar@users.noreply.github.com>
2022-01-12 17:16:29 +0530

    Update RoomBooking.py

--- RoomBooking.py
+++ RoomBooking.py
@@ -10,22 +10,28 @@ class Customer(Hotel): #another class insert main class
     def getData(self):
         super().getData() #to fetch Hotel details
         self._customer_name=input("Customer Name : ") #protected
-        self.__age=input("age : ") #private
+        self.__age=input("Customer Age : ") #private
+        self._address = input("Customer Address:")
 
 
 class Bill(Customer):
 
     def calculateBill(self):#create function and call customer data
         super().getData()
-        flag=int(input("1.One Day 2.Full Day "))
+        flag=int(input("1.Full Day 2.Half Day "))
 
         if(flag==1):
             print("Bill Generated for ",self.name)
             print("Customer name ",self._customer_name) #protected variable accessible
+            print("From ",self._address)
             #print("Age ",self.__age) private variable is not accessible
             print("Stay at Full Day Rs. 2000 , suggested by Hotel Manager")
         elif(flag==2):
-            print("Stay at One Day Rs. 1000 , suggested by Hotel Manager")
+            print("Bill Generated for ",self.name)
+            print("Customer name ",self._customer_name) #protected variable accessible
+            print("from",self._address)
+            #print("Age ",self.__age) private variable is not accessible
+            print("Stay at Half Day Rs. 1000 , suggested by Hotel Manager")
         else:
             print("Wrong choice or No fees")
 
